d60feb5d31cfd97e54d6f53574e6e83f25c417dd
Julian Merkle <juli.merkle@gmail.com>
2018-05-26 15:03:56 +0200

    Vla

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
+//@EnableConfigurationProperties
+//@EntityScan(basePackages={"com.example.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
