5662abd682f26b61bac8146bd863917c77466fd8
fabianblatz <fabian.blatz@hansgrohe.de>
2018-05-26 17:39:46 +0200

    controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.Controller.GraffityController;
+import com.example.demo.Controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -15,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableJpaAuditing
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"com.example.demo"})
+@ComponentScan(basePackageClasses = {GraffityController.class, UserController.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
