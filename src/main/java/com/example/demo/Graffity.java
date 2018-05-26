4ffd6096be8def502ec9fd0b0bf50ac4e259a448
fabianblatz <fabian.blatz@hansgrohe.de>
2018-05-26 15:50:28 +0200

    added graffity class

---
+++ src/main/java/com/example/demo/Graffity.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Graffity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private UUID uuid;
+
+    @OneToMany
+    private User owner;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
