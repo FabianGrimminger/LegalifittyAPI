319b7b0f8815b8f70e8d779c9b919745149af0c6
fabianblatz <fabian.blatz@hansgrohe.de>
2018-05-26 15:13:02 +0200

    hopefully fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -11,8 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableConfigurationProperties
-//@EntityScan(basePackages={"com.example.demo"})
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.example.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
