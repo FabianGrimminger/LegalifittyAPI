99db848c1296d70a1a22ae2c3a5f8068d872a98c
fabianblatz <fabian.blatz@hansgrohe.de>
2018-05-27 09:34:50 +0200

    kill me

--- Legalifitty.py
+++ Legalifitty.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 import generate
 
+
+
+path = os.path.dirname(os.path.abspath(__file__))
 @app.route('/qrcode/<uuid>', methods = ['GET'])
 def download_qrcode(uuid):
-    logo = os.path.join(os.path.dirname(__file__), 'marker-borderless.svg')
+    logo = os.path.join(path, 'marker-borderless.svg')
     png = generate.createQR(uuid, logo)
 
     return Response(png, mimetype='image/png')
@@ -15,19 +18,21 @@ def download_qrcode(uuid):
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join(os.path.dirname(__file__), f.filename))
+        f.save(os.path.join(path, f.filename))
         return 'SUCCESS'
 
 @app.route('/graffiti/<uuid>', methods = ['GET'])
 def download_graffity(uuid):
-    result = ""
     for file in os.listdir(os.path.dirname(__file__)):
-        filename = file.split('.')[0]
-        if filename == uuid:
-            response = send_from_directory(directory=os.path.dirname(__file__), filename=file)
-            return response
+        splits = file.split('.')
+        if len(splits)==2:
+            filename = splits[0]
+            extension = splits[1]
+            if filename == uuid and extension == 'png':
+                response = send_from_directory(directory=path, filename=file)
+                return response
 
-    return result
+    return "ERROR"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
